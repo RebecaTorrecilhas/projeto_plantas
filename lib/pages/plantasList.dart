e73fbe0441ad32771f5656a617b1b5b392e3fb8e
Rebeca Torrecilhas <rebeca.torrecilhas@gmail.com>
2021-03-24 21:36:15 -0300

    Listagem de plantas

--- lib/pages/plantasList.dart
+++ lib/pages/plantasList.dart
@@ -9,27 +9,53 @@ class _PlantasListState extends State<PlantasList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Minhas plantas'),
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(15),
-          children: [
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ListTile(
-                    leading: Image.asset('images/038-botanic.png'),
-                    minVerticalPadding: 30,
-                    title: Text('Sonu Nigam'),
-                    subtitle: Text('Best of Sonu Nigam Song'),
-                    onTap: () => {},
+      appBar: AppBar(
+        title: Text('Minhas plantas'),
+      ),
+      body: ListView.builder(
+          shrinkWrap: true,
+          itemCount: 6,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => {},
+              child: Container(
+                height: 130,
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(color: Colors.grey)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Image.asset('images/038-botanic.png', height: 120),
+                      Padding(
+                        padding: EdgeInsets.only(left: 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Planta ${index + 1}',
+                              style: TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Tempo de irrigação',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                ],
+                ),
               ),
-            )
-          ],
-        ));
+            );
+          }),
+    );
   }
 }
