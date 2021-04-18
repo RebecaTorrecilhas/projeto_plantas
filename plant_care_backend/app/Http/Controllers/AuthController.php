178d091e3e4d9b5af2f3b26fdc9121315c4078ad
Rebeca Torrecilhas <rebeca.torrecilhas@gmail.com>
2021-04-17 23:12:56 -0300

    config routas inicial

---
+++ plant_care_backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+    
+    public function login(Request $request) {
+        echo  'teste routas';
+    }
+
+    public function register(Request $request) {
+        echo  'teste routas';
+    }
+
+    public function forgot(Request $request) {
+        echo  'teste routas';
+    }
+}
--- plant_care_backend/routes/api.php
+++ plant_care_backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('forgot', [AuthController::class, 'forgot']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('teste', function () {
+        echo 'teste auth';
+    });
 });
