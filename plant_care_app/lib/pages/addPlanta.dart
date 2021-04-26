0fb692cf117e02d1bcb0f149758d8830208157b5
Rebeca Torrecilhas <rebeca.torrecilhas@gmail.com>
2021-04-25 23:09:24 -0300

    Finalizado tela de add planta

--- plant_care_app/lib/pages/addPlanta.dart
+++ plant_care_app/lib/pages/addPlanta.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_plantas/constants.dart';
+import 'package:get/get.dart';
 
 class AddPlanta extends StatefulWidget {
   @override
@@ -6,75 +8,160 @@ class AddPlanta extends StatefulWidget {
 }
 
 class _AddPlantaState extends State<AddPlanta> {
-
   final _especie = TextEditingController();
   final _observacao = TextEditingController();
-  //final _irrigacao = TextEditingController();
-  //final _icon = TextEditingController();
+  final _irrigacao = TextEditingController();
+  final _icon = TextEditingController();
+
+  var icone = '';
 
   final _formKey = GlobalKey<FormState>();
+  var constants = Get.put(Constants());
+
+  showEscolherIcon() {
+    List<SimpleDialogOption> items = [];
+
+    constants.imagens.forEach((imagem) {
+      items.add(
+        SimpleDialogOption(
+          child: Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Image.asset(
+              imagem,
+              height: 100,
+            ),
+          ),
+          onPressed: () {
+            _icon.text = imagem;
+            setState(() {
+              icone = imagem;
+            });
+            Get.back();
+          },
+        ),
+      );
+    });
+
+    final SimpleDialog dialog = SimpleDialog(
+      children: items,
+      insetPadding: EdgeInsets.symmetric(
+          vertical: MediaQuery.of(context).size.height / 6),
+    );
+
+    showDialog(context: context, builder: (_) => dialog);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Adicionar Planta'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(children: [
-          Padding(
-            padding: EdgeInsets.all(24),
-            child: TextFormField(
-                controller: _especie,
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.eco),
-                    border: OutlineInputBorder(),
-                    labelText: 'Espécie da Planta'),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Informe a espécie da planta.';
-                  }
-                  return null;
-                }),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
-            child: TextFormField(
-                minLines: 2,
-                maxLines: 6,
-                controller: _observacao,
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.article),
-                    border: OutlineInputBorder(),
-                    labelText: 'Observações'),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Informe as observações';
-                  }
-                  return null;
-                }),
-          ),
-          Expanded(
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              margin: EdgeInsets.all(25),
-              child: ElevatedButton(
-                onPressed: () => {},
-                child: Text(
-                  'Salvar',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
+        appBar: AppBar(
+          title: Text('Adicionar Planta'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Form(
+                  key: _formKey,
+                  child: Column(children: [
+                    GestureDetector(
+                      onTap: () => showEscolherIcon(),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 24, bottom: 12),
+                            child: Text(
+                              'Clique para escolher o icone',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 24.0),
+                            child: Center(
+                              child: Image.asset(
+                                icone != '' ? icone : 'images/038-botanic.png',
+                                height: 100,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 24.0),
+                      child: TextFormField(
+                        controller: _especie,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.eco),
+                            border: OutlineInputBorder(),
+                            labelText: 'Espécie da Planta'),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Informe a espécie da planta.';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 24.0),
+                      child: TextFormField(
+                          controller: _irrigacao,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.opacity),
+                              border: OutlineInputBorder(),
+                              labelText: 'Tempo de irrigação'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Informe o tempo de irrigação.';
+                            }
+                            return null;
+                          }),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 24.0),
+                      child: TextFormField(
+                          minLines: 2,
+                          maxLines: 6,
+                          controller: _observacao,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.article),
+                              border: OutlineInputBorder(),
+                              labelText: 'Observações'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Informe as observações';
+                            }
+                            return null;
+                          }),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      margin: EdgeInsets.all(25),
+                      child: ElevatedButton(
+                        onPressed: () => {},
+                        child: Text(
+                          'Salvar',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(350, 50),
+                        ),
+                      ),
+                    ),
+                  ]),
                 ),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(350, 50),
-                ),
-              ),
-            ),
-          )
-        ]),
-      ),
-    );
+              )
+            ],
+          ),
+        ));
   }
 }
